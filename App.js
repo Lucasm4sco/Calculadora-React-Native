3f9e26a95caf875a1516434996d4f3d0f5033e5c
Lucasm4sco <lmbp202@gmail.com>
2022-10-14 19:42:12 -0300

    adicionando botoes

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Feather } from '@expo/vector-icons'
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', 0, '.', '+/-', '='];
 
   const styles = StyleSheet.create({
     container: {
@@ -18,7 +19,8 @@ const App = () => {
       padding: 10
     },
     resultText: {
-      fontSize: 25
+      fontSize: 25,
+      color: darkMode? 'white' : 'black'
     },
     themeButton: {
       backgroundColor: darkMode ? '#7b8084' : '#e5e5e5',
@@ -28,7 +30,21 @@ const App = () => {
       width: 50,
       borderRadius: 25,
       alignSelf: 'flex-start',
-      bottom: 120
+      bottom: 120,
+    },
+    buttons: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    button: {
+      backgroundColor: darkMode ? '#3f4d5b' : '#e5e5e5',
+      width: '25%',
+      minHeight: '15.7%',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    textButton: {
+      fontSize: 20
     }
   });
 
@@ -47,7 +63,15 @@ const App = () => {
         <Text style={styles.resultText}>3 + 2 = 5</Text>
       </View>
       <View style={styles.buttons}>
-
+        {buttons.map(button =>
+          button === '=' ?
+          <TouchableOpacity key={button} style={styles.button}>
+            <Text style={styles.textButton}>{button}</Text>
+          </TouchableOpacity> :
+          <TouchableOpacity key={button} style={styles.button}>
+            <Text style={styles.textButton}>{button}</Text>
+          </TouchableOpacity>
+        )}
       </View>
     </View>
   );
