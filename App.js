dacefa8fda55f407c149565b9816f1d81ea84c37
Lucasm4sco <lmbp202@gmail.com>
2022-10-17 00:01:27 -0300

    refatoração e inicio da logica

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { StyleSheet, Text, View, StatusBar, TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import Buttons from './src/components/Buttons';
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
+  const [numberHostory, setNumberHistory] = useState('');
+  const [resultText, setResultText] = useState('');
 
   const styles = StyleSheet.create({
     container: {
@@ -37,10 +39,8 @@ const App = () => {
       flexDirection: 'row',
       flexWrap: 'wrap',
       height: '65%',
-      backgroundColor: !darkMode ? '#555' : '#e5e5e5',
     },
     button: {
-      backgroundColor: darkMode ? '#3f4d5b' : '#e5e5e5',
       width: '25%',
       height: '20%',
       alignItems: 'center',
@@ -50,7 +50,7 @@ const App = () => {
     },
     textButton: {
       fontSize: 22,
-      color: darkMode? 'white' : 'black'
+      color: darkMode? '#f5f5f5' : 'black'
     },
     numbers: {
       backgroundColor: darkMode? '#303946' : '#fff'
@@ -59,9 +59,32 @@ const App = () => {
       backgroundColor: darkMode? '#414853' : '#ededed'
     },
     equalSign: {
-      backgroundColor: '#9DBC7B'
+      backgroundColor: '#9DBC7B',
+      color: '#fff'
     }
   });
+
+  const buttonEvent = useCallback((text) => {
+    switch(text){
+      case 'AC':
+        setNumberHistory('');
+        setResultText('');
+        break;
+      case 'DEL':
+        setResultText(resultText.substring(0, resultText.length - 1));
+        break;
+      case '=':
+        setNumberHistory(resultText + '=');
+        const result = eval(resultText);
+        result? setResultText(result) : setResultText('0');
+        break;
+      case '+/-': 
+        //
+      default:
+        setResultText(resultText+text);
+        break;
+    }
+  }, [resultText])
 
   return (
     <View style={styles.container}>
@@ -75,9 +98,10 @@ const App = () => {
             color={darkMode?  'white' : 'black'} 
           />
         </TouchableOpacity>
-        <Text style={styles.resultText}>3 + 2 = 5</Text>
+        <Text style={styles.numberHostory}>{numberHostory}</Text>
+        <Text style={styles.resultText}>{resultText}</Text>
       </View>
-      <Buttons styles={styles}/>
+      <Buttons styles={styles} buttonEvent={buttonEvent}/>
     </View>
   );
 }
--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import { buttons } from "../../caracteres";
 import { View, TouchableOpacity, Text } from "react-native";
 
-const Buttons = ({styles}) => (
+const Buttons = ({styles, buttonEvent}) => (
     <View style={styles.buttons}>
         {buttons.map(button =>
           <TouchableOpacity 
             key={button.caractere} 
-            style={
-                {...styles.button,
-                 ...styles[button.estilo]
-                }}>
+            style={{
+                ...styles.button,
+                ...styles[button.estilo]
+                }}
+            onPress={() => buttonEvent(button.caractere)}
+                >
             <Text style={styles.textButton}>
               {button.caractere}
             </Text>
